bec8698f04507dded45dc89080bdc5e1ff3096b9
Sezn1 <146748513+Sezn1@users.noreply.github.com>
2023-10-07 12:19:38 +0300

    Add files via upload

---
+++ PhoneBook.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinkedList_PhoneBook;
+import java.util.*;
+public class PhoneBook {
+    
+    static LinkedList<Contact> contactList = new LinkedList<Contact>();
+    static LinkedList<Event> eventList = new LinkedList<Event>();
+
+    public static void main(String[]args){
+        
+    }
+        public static<T> void AddContact(Contact C){
+          if (contactList.isEmpty())  
+              contactList.insert(C);
+          contactList.findFirst();
+          while(contactList.retrieve()!=null && !contactList.retrieve().getphone().equals(C.getPhonenumber()) )
+          {
+              if(contactList.retrieve().compareTo(C)<0) {
+                  contactList.insert(C);
+              }
+              else if (contactList.retrieve().compareTo(C)>0){
+                  Contact contactToBeAfterC=contactList.retrieve();
+                  contactList.update(C);
+                  contactList.insert(contactToBeAfterC);
+              }
+              
+              else if (contactList.retrieve().copareTo(C)==0){
+                  System.out.println("the contact is exist");
+                  return;
+              } contactList.findNext();
+          }
+          if(contactList.retrieve().getphone().equals(C.getPhonenumber())
+                  {
+              
+              System.out.println("the contact is exist");
+        }       
+              }
+        
+              
+             public void deleteContact(String S ) {
+                 if (contactList.isEmpty()){
+                     System.out.println("Empty List");
+                     return;
+                 }
+                 contactList.findFirst();
+                 while(!contactList.last())
+                 {
+                     if(contactList.retrieve().getcontactName().equals(S)){
+                         contactList.remove();
+                         System.out.println(S+"contact is deleted");
+                         return
+                         }
+                     contactList.findNext();
+                 }
+                 if(contactList.retrieve().getcontactName().equals(S)){
+                         
+                         contactList.remove();
+                 System.out.println(S+"contact is deleted");
+             }
+             else
+             System.out.println("the contact is not deleted");
+}
+
+                     
+ 
+                 
+
+             
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+              
+              
+              
+          
+                  
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                     
+            
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+    
+
