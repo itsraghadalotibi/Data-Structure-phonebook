8ff0b13a1ab3c36dd85312335b342bcd3b9af8f8
Raghad <raghadalotibi939@gmail.com>
2023-10-07 15:34:10 +0300

    double linked list class
    double linked list class

---
+++ DoubleLinkedList.java
@@ -0,0 +1,17 @@
+public class DoubleLinkedList<T> {
+     private Node<T> head;
+    private Node<T> current;
+public DoubleLinkedList() { 
+        head = current = null;
+    }
+    public boolean empty() { 
+        return head == null;
+    }
+    public boolean last() {
+return current.next == null;
+}
+public boolean first() {
+    return current.previous == null;
+    }
+    
+}
